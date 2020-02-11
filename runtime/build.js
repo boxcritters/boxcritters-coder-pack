44a1bf14c8ec3c3adac5060a8e7cf8ce9b6e521b
Tumble <tumblegamer@gmail.com>
2020-02-11 19:04:52 +0000

    install gump

--- runtime/build.js
+++ runtime/build.js
@@ -4,3 +4,4 @@ const fs = require('fs-extra')
 const http = require('http');
 var extract = require('extract-zip');
 const path = require("path");
+
---
+++ src/modname/modinfo.js
@@ -0,0 +1,5 @@
+module.exports = {
+	name:"modname",
+	version:"0",
+	main:"hello.js"
+}
