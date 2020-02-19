e856c892afa1b4e3664f2ca05e082b1d0dd34c02
Tumble <tumblegamer@gmail.com>
2020-02-19 00:15:46 +0000

    Update bcp.js

--- src/bcp/bcp.js
+++ src/bcp/bcp.js
@@ -4,5 +4,5 @@ socketPath = 'https://boxcritters.herokuapp.com';
 // ---------------------------------------------
 console.log("[BOX CRITTERS CODERPACK]");
 console.log("-----------------------------------");
-console.log("Created by the Box Critters\nModding Community");
+console.log("Created by the BC Modding Community");
 console.log("-----------------------------------");
